1b45dbb3fa7e6d4c680a82f67b21e5623671f8b1
Willard Roseberry <wgrroseberry@fullsail.edu>
2013-05-11 11:59:00 -0600

    "Working on the Array in the Variables block of code. Curently in the say console.log part"

--- Roseberry_Willard_Project_two.js
+++ Roseberry_Willard_Project_two.js
@@ -1,27 +1,35 @@
 //alert("JavaScript works!");
 
-var walk = "\"RiverParkWay\" ";
-var myFamilyName = "Roseberry's";
-var numOfMiles = 2;
-var strollerExcapes = 6;
+    var walk = "\"RiverParkWay\" ";
+    var myFamilyName = "Roseberry's";
+    var numOfMiles = 2;
+    var strollerExcapes = 6;
 
 
+           
+
+
+    var say = function(message){console.log(message);};
+
+    var familyNames = ["Mommy", "Daddy","Sarah","Rosella"];
+    var minutesPerVisit=[45,30,25,40];
+    var iceCreamNames = funtion(whoIceCream)
+    
+    {var whichFamilyMember = familyNames[whoIceCream];
+    say (iceCreamNames)[0]
         
+    }
+    
+    say ("I am working on Array's and funtions");
+
+    var getstring = function ()   
+
+    //m//yArray[0] = "Icecream";
+    //myArray[1] = "girls";
+    //myArray[2] = "Coat"
 
 
-var say = function(message){console.log(message);};
+    //say ("My family loves " + myArray[0] + " exspesialy my " + myArray[1] + " they will even put on a " + myArray[2] + "to get some. Yummy!");
+    say ("I hope this works soon. I have a long way to go");
 
-var family = function () {
-    say ("I am working on Array's and funtions");}
-
- var getstring = function ()   
-
-//m//yArray[0] = "Icecream";
-//myArray[1] = "girls";
-//myArray[2] = "Coat"
-
-
-//say ("My family loves " + myArray[0] + " exspesialy my " + myArray[1] + " they will even put on a " + myArray[2] + "to get some. Yummy!");
-say ("I hope this works soon. I have a long way to go");
-
-family();
+    family();
