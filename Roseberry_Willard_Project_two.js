fa9f5ad8d4c793a4490fa17eaf5fd4f640e487f2
Willard Roseberry <wgrroseberry@fullsail.edu>
2013-05-11 07:58:14 -0600

    Building an Array and assigning data

--- Roseberry_Willard_Project_two.js
+++ Roseberry_Willard_Project_two.js
@@ -5,4 +5,8 @@ var myFamilyName = "Roseberry's";
 var numOfMiles = 2;
 var strollerExcapes = 6;
 
+var myArray = familyData();
 
+myArray[0] = "Icecream";
+myArray[1] = "girls";
+myArray[2] = "Coat"
