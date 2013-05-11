e4d2e2ad89b0e55350ea70c7a206d5435d1381ba
Willard Roseberry <wgrroseberry@fullsail.edu>
2013-05-11 09:10:30 -0600

    "finding out why myFamilyData function is not working it comes out with a not defined error"

--- Roseberry_Willard_Project_two.js
+++ Roseberry_Willard_Project_two.js
@@ -5,13 +5,17 @@ var myFamilyName = "Roseberry's";
 var numOfMiles = 2;
 var strollerExcapes = 6;
 
-var myArray = familyData();
+var myArray = myFamilyData();
 
 var say = function(message){console.log(message);};
-var familyData = function(myFamilyName) 
+varmyFamilyData = function(numOfMiles){ say ("The river parkway")};
+                 
+
+    
+
 myArray[0] = "Icecream";
 myArray[1] = "girls";
 myArray[2] = "Coat"
 
 
-say("My family loves " + myArray[0] + " exspesialy my " + myArray[1] + " they will even put on a " + myArray[2] + "to get some. Yummy!");
+say ("My family loves " + myArray[0] + " exspesialy my " + myArray[1] + " they will even put on a " + myArray[2] + "to get some. Yummy!");
