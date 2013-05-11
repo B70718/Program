4ab4bd288d2de5d25b624747e6d3110aa0f117ce
Willard Roseberry <wgrroseberry@fullsail.edu>
2013-05-11 08:41:10 -0600

    "Building a familyData function inside myArray"

--- Roseberry_Willard_Project_two.js
+++ Roseberry_Willard_Project_two.js
@@ -7,6 +7,11 @@ var strollerExcapes = 6;
 
 var myArray = familyData();
 
+var say = function(message){console.log(message);};
+var familyData = function(myFamilyName) 
 myArray[0] = "Icecream";
 myArray[1] = "girls";
 myArray[2] = "Coat"
+
+
+say("My family loves " + myArray[0] + " exspesialy my " + myArray[1] + " they will even put on a " + myArray[2] + "to get some. Yummy!");
