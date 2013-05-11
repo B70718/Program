5046b3d68277288f224356979ba6bb89f5ce870f
Willard Roseberry <wgrroseberry@fullsail.edu>
2013-05-11 10:12:06 -0600

    "Working on Array's and Funtions"

--- Roseberry_Willard_Project_two.js
+++ Roseberry_Willard_Project_two.js
@@ -5,17 +5,23 @@ var myFamilyName = "Roseberry's";
 var numOfMiles = 2;
 var strollerExcapes = 6;
 
-var myArray = myFamilyData();
+
+        
+
 
 var say = function(message){console.log(message);};
-varmyFamilyData = function(numOfMiles){ say ("The river parkway")};
-                 
 
-    
+var family = function () {
+    say ("I am working on Array's and funtions");}
 
-myArray[0] = "Icecream";
-myArray[1] = "girls";
-myArray[2] = "Coat"
+ var getstring = function ()   
+
+//m//yArray[0] = "Icecream";
+//myArray[1] = "girls";
+//myArray[2] = "Coat"
 
 
-say ("My family loves " + myArray[0] + " exspesialy my " + myArray[1] + " they will even put on a " + myArray[2] + "to get some. Yummy!");
+//say ("My family loves " + myArray[0] + " exspesialy my " + myArray[1] + " they will even put on a " + myArray[2] + "to get some. Yummy!");
+say ("I hope this works soon. I have a long way to go");
+
+family();
