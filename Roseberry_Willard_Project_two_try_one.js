50f62332adcc73e5d80e60ee30d1996b1f4f89f4
Willard Roseberry <wgrroseberry@fullsail.edu>
2013-05-12 22:20:27 -0600

    "Makeing the code look nice"

--- Roseberry_Willard_Project_two_try_one.js
+++ Roseberry_Willard_Project_two_try_one.js
@@ -1,24 +1,6 @@
 //Willard Roseberry
 
-     /* var goodBehavior = true;
-      var walk = "\"RiverParkWay\" ";
-      var myFamilyName = "Roseberry's";
-      var numOfMiles = 10;
-      var numberOfMesses = 5;
-      var strollerExcapes = 2;
-      var daughter = "\"Rosella\" ";
-      var bottlesOfWater = 50;   
-      var say = function(message) console.log(message);
-      
-      reverse = false;
-
-      var ourSmartPhones = new Array();
-        ourSmartPhones [0] = "Note 2";
-        ourSmartPhones [1] = "Galaxcy one";
-        ourSmartPhones [2] = "Samsung Galaxcy two";
-        ourSmartPhones [3] = "Samsung Exsibit"; */
-        
-        //Writing in psudeo code first to figure out the logic
+     
         
         // Bottles, Apples and aranges for logic
         var say = function(message) console.log(message);
@@ -32,8 +14,7 @@
         ourSmartPhones [1] = "Galaxcy one";
         ourSmartPhones [2] = "Samsung Galaxcy two";
         ourSmartPhones [3] = "Samsung Exsibit";
-        
-        
+       
         function myProcedure(apples , pears) 
         {
             
@@ -49,21 +30,21 @@
             
             say (apples + " apples is the same amount as " + pears + " pears")
             
-            //code
+            //puting a string together for the procedure
         }
         
         else if (apples < pears)
         {
             
             say (apples + "apples is less than pears" + pears)
-            //code
+            //compareing apples to pears
         }
      
             else if (apples > pears)
         {
             
             say (apples + "apples is greater than pears + pears")
-            //code
+            //If apples greater than pears
         }
         //Boolan funtion
            function chase( apples , pears)
@@ -82,14 +63,12 @@
             Rosella = true
             
              say ( apples + " and " + pears );
-           
         
            say ( "It is " + apples + " that " + Rosella + "apples " + "is equal to " + Rosella + "pears" + "my Boolan")
             
           //Boolan funtion
            
         }
-    
         
         else if (apples <= pears && Rosella)
         
@@ -100,7 +79,7 @@
             //Boolan funtion
         }
         
-        else if (apples >= pears && Rosella)
+            else if (apples >= pears && Rosella)
         {
             
             apples = false
@@ -108,232 +87,64 @@
             Rosella = false
             //Boolan funtion
         }
-        var juiceBoxes = 3
+            var juiceBoxes = 3
         
-        function oneNumber(juiceBoxes)
+            function oneNumber(juiceBoxes)
         {
-        return juiceBoxes
+            return juiceBoxes
             //Number funtion
         }
-           var x = 1;
-           var answer = oneNumber
-           say (" We have " + juiceBoxes + " juiceBoxes left");
+            var x = 1;
+            var answer = oneNumber
+            say (" We have " + juiceBoxes + " juiceBoxes left");
           
-          while (juiceBoxes >= 1)
+            while (juiceBoxes >= 1)
         {
             
             juiceBoxes = juiceBoxes - 1;
             say (" We are down to " + juiceBoxes + " juiceBoxes");
            
         }
-        
-        
+               
             if (juiceBoxes === 0)
-        {  say ("Time to buy more juiceBoxes")
+        {   say ("Time to buy more juiceBoxes")
                
             juiceBoxes = 2 * 25;
-             say ("We now have " + juiceBoxes + " In the house")   //code
-             
+            say ("We now have " + juiceBoxes + " In the house")   //code    
         }
-            
             else if (juiceBoxes < 50)
         {   say ("We did not buy enough juiceBoxes")
-                //code
-
+                //Checking to see if enough juiceBoxes 
         }
-        
-      
-          
-           function string(Rosella, juiceBoxes)
-           
+           function string(Rosella, juiceBoxes)  
         {
             say ( Rosella + "and " + juceBoxes + " have a lot in common")
             //string funtion
-           // return string
-    
-        //}
-           say ( "It is "+ Rosella + " that Rosella has " + juiceBoxes + " juiceboxes in the house.")
-         
-            // return string
+        
+            say ( "It is "+ Rosella + " that Rosella has " + juiceBoxes + " juiceboxes in the house.")
         }
-        //50 juiceboxes down to zero
-            
-           var          apples = 5;
-           var          pears = 5;
+            //50 juiceboxes down to zero
            
-           say ("It is " + Rosella + " that " + " Rosella loves to drink " + juiceBoxes + " juiceBoxes.")
-        
+            say ("It is " + Rosella + " that " + " Rosella loves to drink " + juiceBoxes + " juiceBoxes.")
            
-           function array(juiceBoxes, ourSmartPhones)
-           {
+            function array(juiceBoxes, ourSmartPhones)
+        {
             say (juiceBoxes + " and " + ourSmartPhones + " are really nice! ")
-            //code
+            //Inside the array funtion
             
-            return array()
-           
-           } 
-           
-          for (var i = 0;
-               i < ourSmartPhones;
+            return array()       
+        } 
+            var a = (2 * 5);
+            for (var i = 0;
+            i < ourSmartPhones;
                i++)
-          say (ourSmartPhones + "our Smart Phones. ")
-          {
-            say("our smart phones are working " +  ourSmartPhones  + "Working ")
+          
+            say (ourSmartPhones + "our Smart Phones. ")
+            say (i + " is the number " + a + " is 10 times the number ") 
+        {
+            say("our smart phones are working " +  ourSmartPhones  + " Working ")
             //code
             
-          }
-          
-        
-          
-        
-          
-          
-        
-        
-        
-      
-         
-      /*  function both (numOfMiles, numberOfMesses)
-     {
-        say ("Rosella has ran " + numOfMiles + " and made " + numberOfMesses + " number of messes")
-        return  numberOfMesses + numOfMiles
-        //returning numOfMiles and numberOfMesses
-     }
-     
-      var results = both (numOfMiles,numberOfMesses)
-      
-     
-        say (" Rosella ran " + numOfMiles + " miles and had " + numberOfMesses +" stroller excapes")
-     
-       // boolean funtion
-      if (bottlesOfWater >= numOfMiles && numberOfMesses)
-      {    
-         
-         say (" Rosella has drank to much water and pored less water on the floor. ")
-      }
-          
-          else if (bottlesOfWater <= numOfMiles && numberOfMesses) 
-            //code
-          
-     {
-          say (" Rosella has drank less water and pored more water on the floor.")
-          
-     }
-     
-     function water (bottlesOfWater, numberOfMesses)
-     {
-        return  bottlesOfWater, numberOfMesses
-        //returning numOfMiles and strollerExcapes
-     }
-     
-     var results = both (numOfMiles,numberOfMesses)
-     
-     if ( goodBehavior === true && numberOfMesses === 0)
-      
-     {
-     
-     say (" It is "+ goodBehavior + " that Rosella is asleep because she is running  " + numOfMiles + " miles and is makeing " + numberOfMesses +" messes.")
-     
-     }
-      else if (goodBehavior === false && numOfMesses > 0)
-      
-      say (" Rosella it is + goodBehavior + that Rosella is haveing good behavior")
-     
-     function good(strollerExcapes) {
-        //code
-     }
-     
-     function offOrOn(bottlesOfWater,numberOfMesses)
-     {
-      return bottlesOfWater + numberOfMesses  //code
-     }
-     
-     if (bottlesOfWater && numberOfMesses < 500 && true)
-     {say(" It is " + goodBehavior + "that " + bottlesOfWater  + " and numberOfMesses+  make life easyer to handle.")
-        //code
-     }
-     else if         //code
-     
-     (bottlesOfWater && numberOfMesses > 500 && false)
-     {say(" It is false that " +  bottlesOfWater + " bottlesOfWater are easy to handle with Rosella")
-                                      
-     }                                
-                                    
-     
-     
-     
-     
-     
-     
-        //code
-        
-          
-     
-      /*function RoselaSaysHi(name){say(" Hello, " + name)
-      return(name);
-      }
-      
-      RoselaSaysHi();  */
-     
-        
-     
-                   
-            //code
-        
-     
-     
-     
-   
+        }
     
-        //code
      
-        
-        
-        //code
-     
-
-/*
-    var say = function(message){console.log(message);
-    
-    return (iceCreamNames)};
-
-    var familyNames = ["Mommy", "Daddy","Sarah","Rosella"];
-    var minutesPerVisit=[45,30,25,40];
-    var iceCreamNames = function(whoIceCream){     
-    
-    var familyNames = minutesPerVisit[whoIceCream],
-    
-    minutesPer = minutesPerVisit[IceCreamNumber];
-    
-    
-    
-  say (familyNames)[0]+"wants us to visit Far better ice cream" + familyNames + " for"  + minutesPerVisit + " minutes. ";
-           for (var minutes = 0; minutes < minutesPer; minutes +=5) {
-            var minutesRemain = minutesPer - minutes;
-            
-            say (" We have " + minutesRemain + " minutes left " + minutes + " minutes have past");
-            //how many minits have past and how many remaining minutes
-           }
-           
-           say ("We finished  " + familyNames + ".");
-    };
-    for (var IceCreamNumber = 0; IceCreamNumber < familyNames.length;
-         IceCreamNumber++) {
-        iceCreamNames(IceCreamNumber);
-        //code
-    };*/
-    
-    
-    //say ("I am working on Array's and funtions")};
-
-    //var getstring = function ()   
-
-    //m//yArray[0] = "Icecream";
-    //myArray[1] = "girls";
-    //myArray[2] = "Coat"
-
-
-    //say ("My family loves " + myArray[0] + " exspesialy my " + myArray[1] + " they will even put on a " + myArray[2] + "to get some. Yummy!");
-   //say ("I hope this works soon. I have a long way to go");   
-
-  
