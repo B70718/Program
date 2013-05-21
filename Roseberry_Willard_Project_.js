a9d2e82b44bbf54875a9ac92e625145b5a30a75c
Willard Roseberry <wgrroseberry@fullsail.edu>
2013-05-20 22:03:58 -0600

    "Fixed my return function in my Number Argument.

--- Roseberry_Willard_Project_.js
+++ Roseberry_Willard_Project_.js
@@ -19,7 +19,7 @@
                       
             
       
- var familyName = function (username)
+         var familyName = function (username)
 {
  var procedure = username;
     ///////// Method: Procedure ////////////////////////////////
@@ -40,21 +40,143 @@ else if (procedure == daddy)
 else if (procedure == mommy)
 {
        console.log ("Welcome Mommy");
-}
-else if (procedure == "not defined")
-{
-    
-      console.log(" Please try again the wrong user name has been used");
 }     
-}  
+}
+///////////// Number Argument /////////////////////
+           
+     var rosellaOutside = function (permissionFromParents){
+	var time = 61;   // time and 30 minits
+	 var minits = permissionFromParents;
+	if (time < 15) {
+		console.log(" Rosella says thats not enough time!");
+		//code
+	}
+	if  (time === 30) {
+		console.log(" Rosella says. Thanks for the 30 minits but I want to stay out longer!");
+		//code
+	}
+	if (time >= 60) {
+		console.log(" Rosella stayed out for over an hour. She is very tired and falls asleep. It is time to put her to bed");
+		
+		
+	}
+	return time;
+     }
+     
+     console.log ("Please tell me the" + myTimeValue + " Thanks");
+	
+	////// Argument Boolean/////
+	
+	      var rosellaBoard = function (makeingMess , outSideWithOutPermision)
+	{
+	      var papersBooks	= makeingMess;   // Rosella gets board and finds papers or books to make a mess and 
+	      var unLockDoor = outSideWithOutPermision;
+	      if (papersBooks === true)
+	{
+		
+	    console.log(" It is " + papersBooks + " that Rosella is making a mess with papers and books because she is board");
+	    console.log(" It is " + unLockDoor + " that Rosella is staying inside to play");
+	}
+	else
+	{
+	    console.log(" It is " + papersBooks + " that Rosella is makeing a mess" + " She is inside being good");
+	    console.log(" It is " + unLockDoor + " that Rosella is inside." + " She is outside runing around");
+	   
+	}
+	}
+	
+	////////////// while loop //////////////////
+	var a = 1;
+	while (a < 5)
+	{
+	 console.log( " Rosella makes " + a + " messes when she is board.");
+	 a ++;
+	}
+
+
+
+        /////////// Property Boolean /////////////////////////////k
+        
+	   
+	 
+	   var makeingMess = true;
+	   var outSideWithOutPermision = true;
+	  
+	   var putThingsOnFloor = makeingMess;
+	   var extraFreedom = outSideWithOutPermision;
+	   
+	   unLockDoor = true;
+            if (putThingsOnFloor && extraFreedom)
+        {
+	    
+            console.log(" Rosella wants to go and play and it is " + putThingsOnFloor  + " that she is making a mess");
+            console.log(" Rosella wants to go outside and it is " + extraFreedom + " that she left with out permission");
+
+           // If the value parameter is omitted or is 0, -0, null, false, Nan,undefined, or the empty string (""), the object has an initial value of false.
+        }
+           else (putThingsOnFloor != unLockDoor)
+	{
+	     console.log(" It is " + putThingsOnFloor+ " that Rosella is makeing a mess or it is " + unLockDoor + " that Rosella unlocked the door");
+        }
+//////////////////////////  Augument Number        //////////////////////////////////////////////////
+          var car = true;  
+          var juiceBoxes = 3
+          var goingForRide =  function(juiceBoxes, car)
+        { 
+            
+            var x = 1;
+            var ride = goingForRide;
+            console.log (" We have " + juiceBoxes + " juiceBoxes left and it is " + car + " that we are going for a ride in the car");
+          
+            while (juiceBoxes >= 1)
+        { 
+            juiceBoxes = juiceBoxes - 1;
+            console.log (" We are down to " + juiceBoxes + " juiceBoxes");  
+        }
+            if (juiceBoxes === 0)
+        {   console.log ("Heading to the store now to buy 50 more juiceBoxes")
+               
+            juiceBoxes = 2 * 25;
+            console.log ("We now have " + juiceBoxes + " In the house")   //code    
+        }
+            else if (juiceBoxes < 50)
+        {   console.log ("We did not buy enough juiceBoxes")     
+        }
+          return juiceBoxes, car;
+	}
+
+
+
+
    //// Main code/////////    
-      familyName(daddy);
+      familyName(daddy);   // make an array or object put in a for loop and call all four family members.
        
       familyName( mommy);
       
       familyName( sarah );
       
       familyName( rosella );
+      
+      rosellaOutside( 15 );
+      
+      rosellaBoard( true , true);
+      
+       console.log ("The length of the rosellaBoard propertiey is " + rosellaBoard.length)
+       
+       goingForRide(3, true);
+       
+       var myTimeValue = rosellaOutside(61);
+       
+       console.log("Rosella stayed outside for " + myTimeValue + " minits. Now she is very tired");
+       
+       
+    
+      
+    
+      
+     
+      
+      
        
      
       
