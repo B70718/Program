47884f2cf489e3ef17081e315f75f4e5d5393f42
Willard Roseberry <wgrroseberry@fullsail.edu>
2013-05-29 23:23:28 -0600

    "Finished the   Array funtion that picks the the largest number closes to the one given"

--- Roseberry_Willard_Project_4.js
+++ Roseberry_Willard_Project_4.js
@@ -5,7 +5,15 @@
  * Project 4
  * Library of Functions
  */
- 
+  // Global Variables
+  
+         var space = " ";
+         var comma = ",";
+         var tempestString = "Oh brave new world that has such people in it.";
+         var monthString = "Jan,Feb,Mar,Apr,May,Jun,Jul,Aug,Sep,Oct,Nov,Dec";
+  // End of Global Variables 
+    
+         
  var myLibrary = function(){
  
  // Begining of Problem number 1 //  
@@ -86,7 +94,7 @@
         }
         
         // End of Problem number 3
-	// Start of Problem number 4	
+	// Start of Problem number 4//	
        // First letter on each words gets a capital letter. 
       // Seting local variables               
              var myCap = function (capForMe){
@@ -101,8 +109,8 @@
           return newOneString;
                 
         }
-        // End of Problem number 4
-        // Start of Problem number 5
+        // End of Problem number 4//
+        // Start of Problem number 5//
         
             var myStringSeperater = function (listOfString, seperator, inPlaceOf){
                  var answer = [];
@@ -118,24 +126,25 @@
             }
             
            
-            
-        // Start of Problem number 6
+         // End of Problem number 5 //   
+        // Start of Problem number 6 //
         // Find the differents between two dates
         
-    var dateFunction = function (one,two) {
-		var results = [];
-		var difference = (date1 > date2) ? date1 - date2 : date2 - date1;
-		results[3] = difference / 1000;
-		results[2] = results[3] / 60;
-		results[1] = results[2] / 60;
-		results[0] = results[1] / 24;
-		return results;
+                var dateFunction = function (one,two) {
+                   // using an array to store the data from four differnt dates.
+		var date1 = new Date("6/17/2000");
+                var date2 = new Date("05/29/2013");
+                var timeDiff = Math.abs(date2.getTime() - date1.getTime());
+                var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24)); 
+                 return diffDays;
 
     };
             
-            
+         // End of Problem number 6 //
+         // Start of Problem number 7 //
         // Sending a string into the program and return it as a number
         
+       
         var myStringNumber = function (string){
             string = "456";
             
@@ -143,90 +152,99 @@
             
             return string;
         }
-
-            
-            
-            
-            
-            
-            
-            
-            
         
-            
-            
-            
-            
-            
-            
-            
-            
-        
+         // End of Problem number 7 //
+         // Start of Problem number 8 //
+         
+         /* Given a string that is a list of things separated by a given string, as well as another//
+         
+        string separator, return a string with the first separator changed to the second: “a,b,c” +
+          “,” + “/” --> “a/b/c”. */
+         var mySplitString = function (stringWithComma, replaceSeparator, tempestString){
+               var stringsArray = stringWithComma.split(replaceSeparator);
+               
+               for (var i = 0; i < stringsArray.length; i++)
+                console.log(stringsArray[i] + " /");
+               return stringsArray;     
+         }
+         
+         // End of Problem number 8 //
+         // Start of Problem number 9//
+         
+        var smallAndGreat = function(a){
+         
+         {var t=this,r=Number.POSITIVE_INFINITY,i;
+	for (i=0;i<t.length;i++) if (a<t[i] && t[i]<r) r=t[i];
+	return r;
+                   }
 
-        
-
-   
-
-
-
-
-
-
-
-
-                        
-                        
-   
-
-          
-                
-                
+           var arr=[1,6,12,14],n=5;
+     
+        }
          
          
-
-
+         
     return {
         "myCap": myCap,
         "myPhoneString": myPhoneString,
         "myWorkingEmailString": myWorkingEmailString,
         "myUrl": myUrl,
         "myStringSeperater": myStringSeperater,
-        "myStringNumber": myStringNumber
+        "myStringNumber": myStringNumber,
+        "mySplitString": mySplitString,
+        "dateFunction": dateFunction,
+        "smallAndGreat": smallAndGreat
     }   
  } // end myLibrary
         
 //main code
 var newLib = new myLibrary();
 
-console.log("small and " + newLib.myCap("hope this working. now"));
+console.log("Number 1 Problem small and Large. " + newLib.myCap("we went to the park and had some fun"));
 
          // Phone number test
-console.log("Phone Number " + newLib.myPhoneString("480-923-3344"));
+console.log("Number 2 Problem Phone Number " + newLib.myPhoneString("480-923-3344"));
 
         // Email test   
-console.log("Email test " + newLib.myWorkingEmailString("wgrroseberry@gmail.com"));
+console.log("Problem 3 Email test " + newLib.myWorkingEmailString("wgrroseberry@gmail.com"));
        
          
        //  myWorking http:// and https://
   
- console.log("My url "+ newLib.myUrl("https://"));
- 
- 
-		 
+ console.log("Problem 4 My url "+ newLib.myUrl("https://"));
+  		 
 	   // Caps the first letter of all the words.
 		    
-console.log("hello is this working " + newLib.myCap("hello there"));
+console.log(" Problem 5 hello is this working " + newLib.myCap("hello there"));
 
           // My string seperator
           
-console.log("My string seperator " + newLib.myStringSeperater("hello , how , are , you"));
+          
+console.log("Problem 6 Differnt days " + newLib.dateFunction());     ///////////////////////////  
+          
+console.log("Problem 7 My string seperator " + newLib.myStringSeperater("hello" , "how " , " are  you"));
 
          // My string number
 
-console.log("My string number " + newLib.myStringNumber("456"));
-    
-    
-    
+console.log("Problem 8 My string number " + newLib.myStringNumber("456"));
+       
+        // My split string seperator
+            
+console.log("Problem 9 My split string seperator " + newLib.mySplitString(tempestString,space, "hello"));
+console.log("Problem 9 My split string seperator " + newLib.mySplitString(tempestString));
+console.log("Problem 9 My split string seperator " + newLib.mySplitString( monthString,space, "hello"));
+
+// problem 10
+
+console.log("Problem 8 My string number " + newLib.smallAndGreat(12));
+console.log(" Here is the number " + newLib.smallAndGreat(arr.smallAndGreat(n)); // give 6 
+  
+            
+             
+               
+              
+              
+               
     
     
        
