442fb9e2d3eef12b296be6c33e8c2a3d06010ce5
Willard Roseberry <wgrroseberry@fullsail.edu>
2013-08-24 10:03:13 -0600

    Adding notes tag

---
+++ Roseberry_Willard_project3/Mecai/js/json.js
@@ -0,0 +1,48 @@
+var json = {
+    "favioratePizza": {
+        "groups": ["How often do you eat here?", "Weekly"],
+         "fname": ["First Name", "Rosella"],
+            "lname": ["Last Name", "Roseberry"],
+          "email": ["Email:", "rosella@gmail.com"],
+         "favorite": ["Is Willard pizza shop your faviorate place to buy pizza?", "Yes"],
+    },
+    "favioratePizza1": {
+        "groups": ["How often do you eat here?", "Monthly"],
+         "fname": ["First Name:", "Willard"],
+            "lname": ["Last Name:", "Roseberry"],
+          "email": ["Email:", "rosella@gmail.com"],
+         "favorite": ["Is Willard pizza shop your faviorate place to buy pizza?", "Yes"],
+    },
+    "favioratePizza2": {
+        "groups": ["How often do you eat here?", "Never"],
+         "fname": ["First Name", "Marie"],
+       "lname": ["Last Name", "Roseberry"],
+          "email": ["Email:", "wgrroseberry@gmail.com"],
+         "favorite": ["Is Willard pizza shop your faviorate place to buy pizza?", "Yes"],
+    },
+    "favioratePizza3": {
+        "groups": ["How often do you eat here?", "Ocaionaly"],
+         "fname": ["First Name", "Karan"],
+        "lname": ["Last Name", "Roseberry"],
+         "email": ["Email:", "karan@gmail.com"],
+         "favorite": ["Is Willard pizza shop your faviorate place to buy pizza?", "Yes"],
+         
+     },
+    "favioratePizza4": {
+        "groups": ["How often do you eat here?", "Daily"],
+         "fname": ["First Name", "Sarah"],
+        "lname": ["Last Name", "Roseberry"],
+         "email": ["Email:", "karan@gmail.com"],
+         "favorite": ["Is Willard pizza shop your faviorate place to buy pizza?", "Yes"],    
+     },
+    "favioratePizza5": {
+        "groups": ["How often do you eat here?", "All_the_time"],
+         "fname": ["First Name", "Doug"],
+        "lname": ["Last Name", "Noble"],
+         "email": ["Email:", "fullsail@gmail.com"],
+         "favorite": ["Is Willard pizza shop your faviorate place to buy pizza?", "Yes"],    
+         
+    }
+}
+ 
+
