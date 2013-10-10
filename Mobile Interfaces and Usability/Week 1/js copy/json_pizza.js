aa81a37f12588412d8fc325d349ef9d8d480f372
Willard Roseberry <wgrroseberry@fullsail.edu>
2013-10-09 23:21:28 -0600

    Finished merging gold and bronze

--- Mobile Interfaces and Usability/Week 1/js copy/json_pizza.js
+++ Mobile Interfaces and Usability/Week 1/js copy/json_pizza.js
@@ -1,38 +1,35 @@
-
- 
-
 var json = {
-    "faviorateCreature": {
-        "groups": ["How often do you visit this website?", "Weekly"],
+    "favioratePizza": {
+        "groups": ["How often do you eat here?", "Weekly"],
          "fname": ["First Name", "Rosella"],
             "lname": ["Last Name", "Roseberry"],
           "email": ["Email:", "rosella@gmail.com"],
-           "notes": ["Notes:", "Nice looking Creature!!"],
-         "favorite": ["Do you like this creature shop", "Yes"]
+		   "notes": ["Notes:", "Awesome Pizza!!"],
+         "favorite": ["Is Willard pizza shop your faviorate place to buy pizza?", "Yes"],
     },
     "favioratePizza1": {
         "groups": ["How often do you eat here?", "Monthly"],
          "fname": ["First Name:", "Willard"],
             "lname": ["Last Name:", "Roseberry"],
           "email": ["Email:", "rosella@gmail.com"],
-           "notes": ["Notes:", "Awesome Pizza!!"],
-         "favorite": ["Is Willard pizza shop your faviorate place to buy pizza?", "Yes"]
+		   "notes": ["Notes:", "Awesome Pizza!!"],
+         "favorite": ["Is Willard pizza shop your faviorate place to buy pizza?", "Yes"],
     },
     "favioratePizza2": {
         "groups": ["How often do you eat here?", "Never"],
          "fname": ["First Name", "Marie"],
        "lname": ["Last Name", "Roseberry"],
           "email": ["Email:", "wgrroseberry@gmail.com"],
-           "notes": ["Notes:", "Awesome Pizza!!"],
-         "favorite": ["Is Willard pizza shop your faviorate place to buy pizza?", "Yes"]
+		   "notes": ["Notes:", "Awesome Pizza!!"],
+         "favorite": ["Is Willard pizza shop your faviorate place to buy pizza?", "Yes"],
     },
     "favioratePizza3": {
         "groups": ["How often do you eat here?", "Ocaionaly"],
          "fname": ["First Name", "Karan"],
         "lname": ["Last Name", "Roseberry"],
          "email": ["Email:", "karan@gmail.com"],
-          "notes": ["Notes:", "Awesome Pizza!!"],
-         "favorite": ["Is Willard pizza shop your faviorate place to buy pizza?", "Yes"]
+		  "notes": ["Notes:", "Awesome Pizza!!"],
+         "favorite": ["Is Willard pizza shop your faviorate place to buy pizza?", "Yes"],
          
      },
     "favioratePizza4": {
@@ -40,138 +37,141 @@ var json = {
          "fname": ["First Name", "Sarah"],
         "lname": ["Last Name", "Roseberry"],
          "email": ["Email:", "karan@gmail.com"],
-          "notes": ["Notes:", "Awesome Pizza!!"],
-         "favorite": ["Is Willard pizza shop your faviorate place to buy pizza?", "Yes"]    
+		  "notes": ["Notes:", "Awesome Pizza!!"],
+         "favorite": ["Is Willard pizza shop your faviorate place to buy pizza?", "Yes"],    
      },
     "favioratePizza5": {
         "groups": ["How often do you eat here?", "All_the_time"],
          "fname": ["First Name", "Doug"],
         "lname": ["Last Name", "Noble"],
          "email": ["Email:", "fullsail@gmail.com"],
-          "notes": ["Notes:", "Awesome Pizza!!"],
-         "favorite": ["Is Willard pizza shop your faviorate place to buy pizza?", "Yes"]
-            },
+		  "notes": ["Notes:", "Awesome Pizza!!"],
+         "favorite": ["Is Willard pizza shop your faviorate place to buy pizza?", "Yes"],
+		    },
     "favioratePizza6": {
         "groups": ["How often do you eat here?", "All_the_time"],
          "fname": ["First Name", "Rosella"],
         "lname": ["Last Name", "Noble"],
          "email": ["Email:", "fullsail@gmail.com"],
-          "notes": ["Notes:", "Awesome Pizza!!"],
-         "favorite": ["Is Willard pizza shop your faviorate place to buy pizza?", "Yes"]   
-          },
+		  "notes": ["Notes:", "Awesome Pizza!!"],
+         "favorite": ["Is Willard pizza shop your faviorate place to buy pizza?", "Yes"],   
+		  },
     "favioratePizza7": {
         "groups": ["How often do you eat here?", "All_the_time"],
          "fname": ["First Name", "Mike"],
         "lname": ["Last Name", "Noble"],
          "email": ["Email:", "fullsail@gmail.com"],
-          "notes": ["Notes:", "Awesome Pizza!!"],
-         "favorite": ["Is Willard pizza shop your faviorate place to buy pizza?", "Yes"]   
-          },
+		  "notes": ["Notes:", "Awesome Pizza!!"],
+         "favorite": ["Is Willard pizza shop your faviorate place to buy pizza?", "Yes"],   
+		  },
     "favioratePizza8": {
         "groups": ["How often do you eat here?", "All_the_time"],
          "fname": ["First Name", "Marie"],
         "lname": ["Last Name", "Noble"],
          "email": ["Email:", "fullsail@gmail.com"],
-          "notes": ["Notes:", "Awesome Pizza!!"],
-         "favorite": ["Is Willard pizza shop your faviorate place to buy pizza?", "Yes"]   
-          },
+		  "notes": ["Notes:", "Awesome Pizza!!"],
+         "favorite": ["Is Willard pizza shop your faviorate place to buy pizza?", "Yes"],   
+		  },
     "favioratePizza9": {
         "groups": ["How often do you eat here?", "All_the_time"],
          "fname": ["First Name", "Louis"],
         "lname": ["Last Name", "Noble"],
          "email": ["Email:", "fullsail@gmail.com"],
-          "notes": ["Notes:", "Awesome Pizza!!"],
-         "favorite": ["Is Willard pizza shop your faviorate place to buy pizza?", "Yes"]   
-          },
+		  "notes": ["Notes:", "Awesome Pizza!!"],
+         "favorite": ["Is Willard pizza shop your faviorate place to buy pizza?", "Yes"],   
+		  },
     "favioratePizza10": {
         "groups": ["How often do you eat here?", "All_the_time"],
          "fname": ["First Name", "Kay"],
         "lname": ["Last Name", "Noble"],
          "email": ["Email:", "fullsail@gmail.com"],
-          "notes": ["Notes:", "Awesome Pizza!!"],
-         "favorite": ["Is Willard pizza shop your faviorate place to buy pizza?", "Yes"]   
-          },
+		  "notes": ["Notes:", "Awesome Pizza!!"],
+         "favorite": ["Is Willard pizza shop your faviorate place to buy pizza?", "Yes"],   
+		  },
     "favioratePizza11": {
         "groups": ["How often do you eat here?", "All_the_time"],
          "fname": ["First Name", "Mark"],
         "lname": ["Last Name", "Noble"],
          "email": ["Email:", "fullsail@gmail.com"],
-          "notes": ["Notes:", "Awesome Pizza!!"],
-         "favorite": ["Is Willard pizza shop your faviorate place to buy pizza?", "Yes"]   
-          },
+		  "notes": ["Notes:", "Awesome Pizza!!"],
+         "favorite": ["Is Willard pizza shop your faviorate place to buy pizza?", "Yes"],   
+		  },
     "favioratePizza12": {
         "groups": ["How often do you eat here?", "All_the_time"],
          "fname": ["First Name", "Christan"],
         "lname": ["Last Name", "Noble"],
          "email": ["Email:", "fullsail@gmail.com"],
-          "notes": ["Notes:", "Awesome Pizza!!"],
-         "favorite": ["Is Willard pizza shop your faviorate place to buy pizza?", "Yes"]   
-          },
+		  "notes": ["Notes:", "Awesome Pizza!!"],
+         "favorite": ["Is Willard pizza shop your faviorate place to buy pizza?", "Yes"],   
+		  },
     "favioratePizza13": {
         "groups": ["How often do you eat here?", "All_the_time"],
          "fname": ["First Name", "Corinne"],
         "lname": ["Last Name", "Noble"],
          "email": ["Email:", "fullsail@gmail.com"],
-          "notes": ["Notes:", "Awesome Pizza!!"],
-         "favorite": ["Is Willard pizza shop your faviorate place to buy pizza?", "Yes"]   
-          },
+		  "notes": ["Notes:", "Awesome Pizza!!"],
+         "favorite": ["Is Willard pizza shop your faviorate place to buy pizza?", "Yes"],   
+		  },
     "favioratePizza14": {
         "groups": ["How often do you eat here?", "All_the_time"],
          "fname": ["First Name", "Gilbert"],
         "lname": ["Last Name", "Noble"],
          "email": ["Email:", "fullsail@gmail.com"],
-          "notes": ["Notes:", "Awesome Pizza!!"],
-         "favorite": ["Is Willard pizza shop your faviorate place to buy pizza?", "Yes"]   
-          },
+		  "notes": ["Notes:", "Awesome Pizza!!"],
+         "favorite": ["Is Willard pizza shop your faviorate place to buy pizza?", "Yes"],   
+		  },
     "favioratePizza15": {
         "groups": ["How often do you eat here?", "All_the_time"],
          "fname": ["First Name", "Thomas"],
         "lname": ["Last Name", "Noble"],
          "email": ["Email:", "fullsail@gmail.com"],
-          "notes": ["Notes:", "Awesome Pizza!!"],
-         "favorite": ["Is Willard pizza shop your faviorate place to buy pizza?", "Yes"]   
-          },
+		  "notes": ["Notes:", "Awesome Pizza!!"],
+         "favorite": ["Is Willard pizza shop your faviorate place to buy pizza?", "Yes"],   
+		  },
     "favioratePizza16": {
         "groups": ["How often do you eat here?", "All_the_time"],
          "fname": ["First Name", "Samantha"],
         "lname": ["Last Name", "Noble"],
          "email": ["Email:", "fullsail@gmail.com"],
-          "notes": ["Notes:", "Awesome Pizza!!"],
-         "favorite": ["Is Willard pizza shop your faviorate place to buy pizza?", "Yes"]   
-          },
+		  "notes": ["Notes:", "Awesome Pizza!!"],
+         "favorite": ["Is Willard pizza shop your faviorate place to buy pizza?", "Yes"],   
+		  },
     "favioratePizza17": {
         "groups": ["How often do you eat here?", "All_the_time"],
          "fname": ["First Name", "Kaylee"],
         "lname": ["Last Name", "Bomea"],
          "email": ["Email:", "fullsail@gmail.com"],
-          "notes": ["Notes:", "Awesome Pizza!!"],
-         "favorite": ["Is Willard pizza shop your faviorate place to buy pizza?", "Yes"]   
-          },
+		  "notes": ["Notes:", "Awesome Pizza!!"],
+         "favorite": ["Is Willard pizza shop your faviorate place to buy pizza?", "Yes"],   
+		  },
     "favioratePizza18": {
         "groups": ["How often do you eat here?", "All_the_time"],
          "fname": ["First Name", "Katrina"],
         "lname": ["Last Name", "Noble"],
          "email": ["Email:", "fullsail@gmail.com"],
-          "notes": ["Notes:", "Awesome Pizza!!"],
-         "favorite": ["Is Willard pizza shop your faviorate place to buy pizza?", "Yes"]   
-          },
+		  "notes": ["Notes:", "Awesome Pizza!!"],
+         "favorite": ["Is Willard pizza shop your faviorate place to buy pizza?", "Yes"],   
+		  },
     "favioratePizza19": {
         "groups": ["How often do you eat here?", "All_the_time"],
          "fname": ["First Name", "mecai"],
         "lname": ["Last Name", "Noble"],
          "email": ["Email:", "fullsail@gmail.com"],
-          "notes": ["Notes:", "Awesome Pizza!!"],
-         "favorite": ["Is Willard pizza shop your faviorate place to buy pizza?", "Yes"]   
-          },
+		  "notes": ["Notes:", "Awesome Pizza!!"],
+         "favorite": ["Is Willard pizza shop your faviorate place to buy pizza?", "Yes"],   
+		  },
     "favioratePizza20": {
         "groups": ["How often do you eat here?", "All_the_time"],
          "fname": ["First Name", "rosella"],
         "lname": ["Last Name", "Rosella"],
          "email": ["Email:", "fullsail@gmail.com"],
-          "notes": ["Notes:", "Awesome Pizza!!"],
-         "favorite": ["Is Willard pizza shop your faviorate place to buy pizza?", "Yes"]     
+		  "notes": ["Notes:", "Awesome Pizza!!"],
+         "favorite": ["Is Willard pizza shop your faviorate place to buy pizza?", "Yes"],     
          
     }
-};
+}
+ 
+
+
  
 
