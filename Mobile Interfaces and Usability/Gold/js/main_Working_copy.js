45c39b589399f087f9032d3c354a39a8694f283a
Willard Roseberry <wgrroseberry@fullsail.edu>
2013-10-17 20:33:33 -0600

    week3 Gold and Bronze

---
+++ Mobile Interfaces and Usability/Gold/js/main_Working_copy.js
@@ -0,0 +1,73 @@
+var parseCreatureForm = function(data){
+	// uses form data here;
+	console.log(data);
+	};
+
+
+
+
+$(document).ready(function(){
+	
+	var rcform = $('#recordmakeCreature');
+	
+	rcform.validate( {
+		invalidHandler: function(form, validator) {},
+		submitHandler: function() {
+			var data = rcform.serializeArray();
+			parseCreatureForm(data);
+			}
+		});
+	
+});
+
+
+
+
+
+/*
+
+ $('#home').on('pageinit', function(){
+	//code needed for home page goes here
+});	
+		
+$('#index_demo_Gold.html').on('pageinit', function(){
+
+		var mcform = $('#creatureErrors');
+		    myCreatureErrorLink = $('#mycreatureErrorslink')
+		    mcform.validate({
+			invalidHandler: function(form, validator) {
+				myCreatureErrorLink.click();
+			},
+			submitHandler: function() {
+		var data = myForm.serializeArray();
+			storeData(data);
+		}
+	});
+	
+	//any other code needed for addItem page goes here
+	
+});
+
+//The functions below can go inside or outside the pageinit function for the page in which it is needed.
+
+var autofillData = function (){
+	 
+};
+
+var getData = function(){
+
+};
+
+var storeData = function(data){
+	
+}; 
+
+var	deleteItem = function (){
+			
+};
+					
+var clearLocal = function(){
+
+};
+
+*/
